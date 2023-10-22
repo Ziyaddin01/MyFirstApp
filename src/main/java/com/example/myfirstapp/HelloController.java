7082ce683721694181a5e2029567453ccdf016ff
Ziyaddin01 <ziya.huseynov200101@gmail.com>
2023-10-23 00:58:32 +0400

    2 Hisse

--- src/main/java/com/example/myfirstapp/HelloController.java
+++ src/main/java/com/example/myfirstapp/HelloController.java
@@ -1,8 +1,31 @@
 package com.example.myfirstapp;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    private Button authSigInButton;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    void initialize() {
+        authSigInButton.setOnAction(actionEvent -> {
+            System.out.println("Siz daxil ol düyməsinə kliklədiz");
+        });
+    }
 
 }
