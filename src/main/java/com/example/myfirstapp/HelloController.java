95f48b191d9ac7f953200509145b3d3137a16110
Ziyaddin01 <ziya.huseynov200101@gmail.com>
2023-10-23 15:28:13 +0400

    4 Hisse Mysql goshdum

--- src/main/java/com/example/myfirstapp/HelloController.java
+++ src/main/java/com/example/myfirstapp/HelloController.java
@@ -2,9 +2,15 @@ package com.example.myfirstapp;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class HelloController {
@@ -23,8 +29,22 @@ public class HelloController {
 
     @FXML
     void initialize() {
-        authSignInButton.setOnAction(actionEvent -> {
-            System.out.println("Siz daxil ol düyməsinə kliklədiz");
+        loginSignUpButton.setOnAction(actionEvent -> {
+            loginSignUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/example/myfirstapp/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
     }
 
